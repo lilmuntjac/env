c5f2dcaaabec205ee89ed719ca4df8b63ec6b590
Eilspus <recapseason@gmail.com>
2022-11-19 02:15:24 +0800

    update packages

--- package/install-nvtop.sh
+++ package/install-nvtop.sh
@@ -3,12 +3,14 @@
 # extends the sudo timeout for another 5 minutes
 sudo -v
 
+# AMD and Intel Dependencies
+sudo dnf install -y libdrm-devel systemd-devel
 # nvtop and its dependency
 sudo dnf install -y cmake ncurses-devel git gcc-c++
 cd /tmp
 git clone https://github.com/Syllo/nvtop.git
 mkdir -p nvtop/build && cd nvtop/build
-cmake .. -DNVIDIA_SUPPORT=ON -DAMDGPU_SUPPORT=ON
+cmake .. -DNVIDIA_SUPPORT=ON -DAMDGPU_SUPPORT=ON -DINTEL_SUPPORT=ON
 make
 # nvtop install globally on the system
 sudo make install
