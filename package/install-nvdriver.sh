099b5d012c32a9ab570686395e118632ccb26863
lilmuntjac <112652247+lilmuntjac@users.noreply.github.com>
2025-07-20 07:23:06 +0800

    remove ghost option

--- package/install-nvdriver.sh
+++ package/install-nvdriver.sh
@@ -13,4 +13,4 @@ sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-v2-elrepo.org
 
 # Install ELRepo and NVIDIA Proprietary Driver
 sudo dnf install -y https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm
-sudo dnf --enablerepo=elrepo-kmod install -y kmod-nvidia nvidia-x11-drv
+sudo dnf install -y kmod-nvidia nvidia-x11-drv
