b375fc3557d0120e7b207438c94d9572e17419dc
lilmuntjac <112652247+lilmuntjac@users.noreply.github.com>
2025-07-20 07:19:28 +0800

    change to more modern repo

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -12,14 +12,12 @@ cd ${INSTALL_PATH}/package
 
 sudo bash install-nvdriver.sh
 sudo bash install-nvtop.sh
-sudo bash install-cudacudnn.sh
 
 while getopts 'p' flag; do
   case "$flag" in
     p)
     # start the NVIDIA Persistence Daemon
-    sudo cp -f ${INSTALL_PATH}/configs/nvidia-persistenced.service /lib/systemd/system/nvidia-persistenced.service
-    sudo systemctl enable --now nvidia-persistenced
+    sudo systemctl enable --now nvidia-persistenced.service
     ;;
   esac
 done
