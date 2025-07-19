7f237f83b6499c5cbe5b399a7b10f31ea03b8b32
lilmuntjac <112652247+lilmuntjac@users.noreply.github.com>
2025-07-20 07:18:07 +0800

    nvtop from epel

--- package/install-nvtop.sh
+++ package/install-nvtop.sh
@@ -6,14 +6,16 @@ set -eo pipefail
 # extends the sudo timeout for another 5 minutes
 sudo -v
 
+sudo dnf install -y nvtop
+
 # AMD and Intel Dependencies
-sudo dnf install -y libdrm-devel systemd-devel
+# sudo dnf install -y libdrm-devel systemd-devel
 # nvtop and its dependency
-sudo dnf install -y cmake ncurses-devel git gcc-c++
-cd /tmp
-git clone https://github.com/Syllo/nvtop.git
-mkdir -p nvtop/build && cd nvtop/build
-cmake .. -DNVIDIA_SUPPORT=ON -DAMDGPU_SUPPORT=ON -DINTEL_SUPPORT=ON
-make
+# sudo dnf install -y cmake ncurses-devel git gcc-c++
+# cd /tmp
+# git clone https://github.com/Syllo/nvtop.git
+# mkdir -p nvtop/build && cd nvtop/build
+# cmake .. -DNVIDIA_SUPPORT=ON -DAMDGPU_SUPPORT=ON -DINTEL_SUPPORT=ON
+# make
 # nvtop install globally on the system
-sudo make install
+# sudo make install
